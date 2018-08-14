e4328ecde9d8f6ccaad49f1ceb2fbf72ed28d3db
Corey McCown <coreymccown13@gmail.com>
2018-08-14 05:58:23 -0400

    Update server.lua
    Removed debugging tprint function.

--- server.lua
+++ server.lua
@@ -81,18 +81,3 @@ AddEventHandler('salty_crafting:craftItem', function(ingredients)
 		end
 	end
 end)
-
-function tprint (tbl, indent)
-  if not indent then indent = 0 end
-  for k, v in pairs(tbl) do
-    formatting = string.rep("  ", indent) .. k .. ": "
-    if type(v) == "table" then
-      print(formatting)
-      tprint(v, indent+1)
-    elseif type(v) == 'boolean' then
-      print(formatting .. tostring(v))      
-    else
-      print(formatting .. v)
-    end
-  end
-end
